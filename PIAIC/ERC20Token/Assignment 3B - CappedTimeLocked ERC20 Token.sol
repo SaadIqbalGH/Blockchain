a84b2e3ad8744455e834a1bfa408add7880ef90d
Saad Iqbal <63910739+SaadIqbalGH@users.noreply.github.com>
2021-08-29 11:52:02 +0500

    Update Assignment 3B - CappedTimeLocked ERC20 Token.sol
    Changed the OpenZeppelin import links for contract migration

--- PIAIC/ERC20Token/Assignment 3B - CappedTimeLocked ERC20 Token.sol
+++ PIAIC/ERC20Token/Assignment 3B - CappedTimeLocked ERC20 Token.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.8.0;
 //"SPDX-License-Identifier: UNLICENSED"
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "@OpenZeppelin/contracts/utils/math/SafeMath.sol";
+import "@OpenZeppelin/contracts/token/ERC20/ERC20.sol";
+import "@OpenZeppelin/contracts/access/Ownable.sol";
 
 contract CappedLockableToken is ERC20, Ownable {
     
